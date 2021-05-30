f325dfec073dfff2a9825d0beb4bf54ce9198dfa
mitya-me <dmitrymefodyev@gmail.com>
2021-05-30 19:13:09 +0300

    Add normalize styles (styles ver. 1.1)

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -4,9 +4,83 @@ import UbuntuMonoBold from "./scss/fonts/UbuntuMono-Bold.woff";
 import UbuntuMonoRegular from "./scss/fonts/UbuntuMono-Regular.woff";
 
 export const GlobalStyle = createGlobalStyle`
-    html {
+
+* {
+    padding: 0;
+    margin: 0;
+    border: 0;
+}
+*,
+*:before,
+*:after {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+:focus,
+:active {
+    outline: none;
+}
+a:focus,
+a:active {
+    outline: none;
+}
+nav,
+footer,
+header,
+aside {
+    display: block;
+}
+html,
+body {
     height: 100%;
-    }
+    width: 100%;
+    font-size: 100%;
+    font-family: Play;
+    color: #26272d;
+    line-height: 1;
+    font-size: 14px;
+    -ms-text-size-adjust: 100%;
+    -moz-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+}
+input,
+button,
+textarea {
+    font-family: inherit;
+}
+input::-ms-clear {
+    display: none;
+}
+button {
+    cursor: pointer;
+}
+button::-moz-focus-inner {
+    padding: 0;
+    border: 0;
+}
+a,
+a:visited {
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: none;
+}
+ul li {
+    list-style: none;
+}
+img {
+    vertical-align: top;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-size: inherit;
+    font-weight: inherit;
+}
 
     @font-face {
         font-family: 'UbuntuMono';
@@ -23,19 +97,10 @@ export const GlobalStyle = createGlobalStyle`
         font-style: normal;
     }
 
-    body {
-    margin: 0;
-    padding: 0 5px;
-    }
-
-  * {
-    box-sizing: border-box;
-    }
 `;
 
-
 export const Wrapper = styled.div`
-    font-family: 'UbuntuMono', 'Courier New', sans-serif;
+    font-family: "UbuntuMono", "Courier New", sans-serif;
     max-width: 1250px;
     display: flex;
     flex-direction: column;
@@ -111,30 +176,29 @@ export const Wrapper = styled.div`
         align-items: center;
 
         &__stat {
-            
             &-score {
                 font-size: 1.6rem;
                 color: pink;
                 text-align: center;
                 margin: 0 0 10px 0;
 
-                @media(max-width: 450px) {
+                @media (max-width: 450px) {
                     font-size: 20px;
                 }
-                
-                span{
+
+                span {
                 }
             }
 
             &-preload {
                 color: pink;
-                font-family: 'UbuntuMono', 'Monospace';
+                font-family: "UbuntuMono", "Monospace";
             }
         }
 
         &__btn-next {
             margin-top: 15px;
-            font-family: 'UbuntuMono';
+            font-family: "UbuntuMono";
             font-weight: bold;
             cursor: pointer;
             border: none;
@@ -142,7 +206,12 @@ export const Wrapper = styled.div`
             border-radius: 10px;
             padding: 5px 15px;
             font-size: 1.8rem;
-            background: linear-gradient(to right, #A445B2 50%, #ffffff 52%, #FF0066 100%);
+            background: linear-gradient(
+                to right,
+                #a445b2 50%,
+                #ffffff 52%,
+                #ff0066 100%
+            );
             -webkit-background-clip: text;
             background-clip: text;
             -webkit-text-fill-color: transparent;
@@ -150,11 +219,11 @@ export const Wrapper = styled.div`
             background-size: 80%;
             animation: shine 1.8s ease-in-out infinite;
 
-            @media(max-width: 850px) {
-                font-size:  1.5rem;
+            @media (max-width: 850px) {
+                font-size: 1.5rem;
             }
 
-            @media(max-width: 450px) {
+            @media (max-width: 450px) {
                 font-size: 1.2rem;
                 margin-top: 10px;
                 box-shadow: 0 0 5px pink;
